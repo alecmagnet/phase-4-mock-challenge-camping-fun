659eea48a09151c09862330a7a1ff1d8ab1b5eac
Alec Magnet <amagnet@gmail.com>
2021-09-03 12:06:27 -0400

    Camper & signup validate numericality greater or less than

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,6 +1,6 @@
 class Camper < ApplicationRecord
 	validates :name, presence: true
-	validates :age, inclusion: { in: 8..18 }
+	validates :age, numericality: { greater_than: 7, less_than: 19 }
 
 	has_many :signups 
 	has_many :activities, through: :signups
--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,5 +1,5 @@
 class Signup < ApplicationRecord
-	validates :time, inclusion: { in: 0..23 }
+	validates :time, numericality: { greater_than_or_equal_to: 0, less_than: 24 }
 
   belongs_to :activity
   belongs_to :camper
